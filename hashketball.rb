ca29e687c587dbf7f2a8b05e795c972450483605
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-19 01:40:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,14 @@
 require 'pry'
 
 def game_hash
-  {:home=>{},
-  :away=>{}
+  {:home=>{
+    :team_name=>"Brooklyn Nets",
+    :colors=>["Black","White"],
+    :players=>[]
+  },
+  {:away=>{
+    :team_name=>"Charlotte Hornets",
+    :colors=>["Turquoise","Purple"],
+    :players=>[]
   }
 end
