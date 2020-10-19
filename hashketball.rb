d3c00d5a9963e671d61f7a5d67033d51511daa0f
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-19 02:31:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -37,6 +37,28 @@ def game_hash
         :steals=>3,
         :blocks=>1,
         :slam_dunks=>15
+      },
+      {
+        :player_name=>"Mason Plumlee",
+        :number=>1,
+        :shoe=>19,
+        :points=>26,
+        :rebounds=>11,
+        :assists=>6,
+        :steals=>3,
+        :blocks=>8,
+        :slam_dunks=>5
+      },
+      {
+        :player_name=>"Jason Terry",
+        :number=>31,
+        :shoe=>15,
+        :points=>19,
+        :rebounds=>2,
+        :assists=>2,
+        :steals=>4,
+        :blocks=>11,
+        :slam_dunks=>1
       }
     ]
   },
