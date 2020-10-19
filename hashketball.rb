c853ea33343e6a8050876c6ed1bd02c48e9fb68e
Rakshan Hegde <hegde.rakshan@gmail.com>
2020-10-19 02:10:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,10 +14,10 @@ def game_hash
         :assists=>12
         :steals=>3,
         :blocks=>1
-        binding.pry
       }
     ]
   }
+  binding.pry
   :away=>{
     :team_name=>"Charlotte Hornets",
     :colors=>["Turquoise","Purple"],
@@ -25,15 +25,3 @@ def game_hash
   }
 }
 end
-
-|          Stat          | Info | Info |  Info | Info | Info   |
-|:------------------:|:-------------:|:------------:|:------------:|:-------------:|:-------------:|
-| **Player Name**    |  Alan Anderson| Reggie Evans | Brook Lopez  | Mason Plumlee | Jason Terry   |
-| **Number**         | 0             | 30           | 11           | 1             | 31            |
-| **Shoe**           | 16            | 14           | 17           | 19            | 15            |
-| **Points**         | 22            | 12           | 17           | 26            | 19            |
-| **Rebounds**       | 12            | 12           | 19           | 11            | 2             |
-| **Assists**        | 12            | 12           | 10           | 6             | 2             |
-| **Steals**         | 3             | 12           | 3            | 3             | 4             |
-| **Blocks**         | 1             | 12           | 1            | 8             | 11            |
-| **Slam Dunks**     | 1             | 7            | 15           | 5             | 1             |
